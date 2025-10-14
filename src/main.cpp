82cdce02b8f35925b231880d58b2bcdda9022846
FamALouiz <fam@awadlouis.com>
2025-10-14 23:56:54 +0200

    feat: updated main for simple window example

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,21 @@
+#include "game_window/game_window.h"
 #include <GL/glut.h>
 #include <stdio.h>
 
-void display()
-{
-    glClear(GL_COLOR_BUFFER_BIT);
-
-    // Your OpenGL rendering code here
-    glBegin(GL_TRIANGLES);
-    glColor3f(1.0f, 0.0f, 0.0f);
-    glVertex2f(-0.5f, -0.5f);
-    glColor3f(0.0f, 1.0f, 0.0f);
-    glVertex2f(0.5f, -0.5f);
-    glColor3f(0.0f, 0.0f, 1.0f);
-    glVertex2f(0.0f, 0.5f);
-    glEnd();
-    glFlush();
-}
-
 int main(int argc, char **argv)
 {
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-    glutInitWindowSize(800, 600);
-    glutCreateWindow("Hoppy! - Hop to it!");
+    GameWindow *window = GameWindow::getInstance();
 
-    glutDisplayFunc(display);
-    glutMainLoop();
+    bool startFullscreen = true;
+    window->create(argc, argv, 800, 600, "Hoppy - OpenGL Game", startFullscreen);
+
+    window->setDisplayCallback(display);
+    window->setKeyboardCallback(keyboard);
+    window->setSpecialCallback(specialKeys);
+
+    window->run();
+
+    GameWindow::destroy();
 
     return 0;
 }
